f461fb75e88aa0e67bef363e6e400d27ff412ebe
Jenisha-123 <97jaffer@gmail.com>
2024-12-12 01:10:18 -0800

    add test method

--- src/test/java/org/junittt/Sample.java
+++ src/test/java/org/junittt/Sample.java
@@ -1,5 +1,17 @@
 package org.junittt;
 
+import junit.framework.Test;
+
 public class Sample {
+
+	
+	public static void main(String[] args) {
+		
+			
+	}
+	public static void test()
+	{
+		System.out.println("test");
+	}
 
 }
